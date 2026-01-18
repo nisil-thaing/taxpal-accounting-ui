464deda6d3c4a2af9fb6947071be6cef80f206d4
Thai (Nisil) Nguyen <nisil.thaing@gmail.com>
2026-01-18 23:21:00 +0700

    Setup TailwindCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
@@ -10,6 +11,7 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
